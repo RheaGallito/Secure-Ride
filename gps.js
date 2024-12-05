89f7d429a87e19ea53d7a2387d93f30f107775b2
RheaGallito <158656090+RheaGallito@users.noreply.github.com>
2024-12-06 01:48:10 +0800

    Update gps.js

--- gps.js
+++ gps.js
@@ -1,18 +1,64 @@
-<!DOCTYPE html>
-<html lang="en">
-    <meta charset="UTF-8">
-    </title>Google Map Example USing JavaScript</title>
+function initMap() {
 
-    <script src="https://maps.googleapis.com/maps/api/js?key=??????"
+    //Location Marker
+    const markers = [
+        {
+        locationName: 'Crumbs & Dollies',
+        lat:  8.454579,
+        lng: 124.265113,
+        address: '1 Kingly Ct, <br> London,<br> W1B 5PW',
+        },
+        {
+            locationName: 'Gps Update',
+            lat: 8.224558,
+            lng: 124.235687,
+            address: '96 Bermondsey'
+        },
+       
+    ];
 
+    const fehMarker =  'asset/gps.png'
+    const centerMap = { lat: 8.254579, lng:  124.265113};
+    const mapOptions = {
+        center: centerMap,
+        zoom: 20,
+        disbleDefault: true,
+    }
+    const map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+    const infoWindow = new google.maps.InfoWindow({
+        minWidth: 200,
+        maxWidth: 200,
+    });
 
+    function createInfoWindow(marker, infoWindowContent) {
+        google.maps.event.addListener(marker, 'click', function(){
+            infoWindow.setContent(infoWindowContent);
+            infoWindow.open(map, marker);
+        });
+    }
 
-</head>
-<body>
-
-
-
-
-
-</body>
-</html>
+    /**
+    * Loop through all markers
+    */
+    for (let i = 0; i < markers.length; i++) {
+        for (let i = 0; i < markers.length; i++) {
+            const marker = new google.maps.Marker({
+                position: { lat: markers[i]['lat'], lng: markers[i]['lng'] },
+                map: map,
+                icon: fehMarker
+            });
+        
+            const infoWindowContent = `
+                <div class="feh-content">
+                    <h3>${markers[i]['locationName']}</h3>
+                    <address>
+                        <p>${markers[i]['address']}</p>
+                    </address>
+                </div>
+            `;
+        
+            createInfoWindow(marker, infoWindowContent);
+        }
+    }
+      
+}
